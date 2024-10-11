693d6289965c38d0ad664400a3e5977f592dc626
Raz <retraz123@gmail.com>
2024-10-11 10:43:19 +0300

    update allowed origins

--- party_manager/settings.py
+++ party_manager/settings.py
@@ -39,9 +39,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
-
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS').split(',')
 
 ROOT_URLCONF = 'party_manager.urls'
 
