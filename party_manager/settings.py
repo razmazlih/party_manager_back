7bacd78cee3c270eb51b88e25d8192af7c47e739
Raz <retraz123@gmail.com>
2024-10-11 10:50:33 +0300

    update settings to get DEBUG false as false

--- party_manager/settings.py
+++ party_manager/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = config('DEBUG')
+DEBUG = config('DEBUG') == 'True'
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
