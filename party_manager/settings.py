30cb786b1718b4eb32aaf2b001e686edf0c74105
Raz <retraz123@gmail.com>
2024-10-11 10:15:58 +0300

    update settings to make dynamic .env

--- party_manager/settings.py
+++ party_manager/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = True
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 # Installed apps
 INSTALLED_APPS = [
